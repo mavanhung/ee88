98836f1214890857943f28e54db63bba2ee75540
mavanhung <29092461+mavanhung@users.noreply.github.com>
2022-12-12 17:25:50 +0700

    .

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\getResultMb45s::class,
     ];
 
     /**
@@ -25,6 +25,10 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('command:mb45s')
+                    ->timezone('Asia/Ho_Chi_Minh')
+                    ->everyMinute()
+                    ->withoutOverlapping();
     }
 
     /**
